584d79b8bd3e09468d48435ebd05f045d3827b97
amarendra11 <amarendrakishor62@gmail.com>
2022-09-13 22:40:38 +0530

    employee

---
+++ day3emproblem1/day3emproblem1.java
@@ -0,0 +1,14 @@
+package day3emproblem1;
+import java.util.Scanner;
+import java.util.random.
+public class day3emproblem1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int emp = sc.nextInt("random 2");
+        if (emp == 0);
+        System.out.println("employe is persent");
+        else
+        System.out.println("employe is obsent");
+    }
+}
